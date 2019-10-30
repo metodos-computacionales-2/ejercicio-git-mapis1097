279c262a4b45d7f4786ec945a71d2d117fcb326a
Maria Paula <mp.ramos@uniandes.edu.co>
2019-10-30 20:01:13 +0000

    Solucion de la segunda parte del ejercicio

--- 2.cpp
+++ 2.cpp
@@ -6,16 +6,20 @@ using namespace std;
 int main()
 {
     float intuno,intdos, inttres;
-    cout << "Ingrese tres valores enteros: ";
-    cin >> intuno,intdos,inttres;
+    cout << "Ingrese primer valor entero: ";
+    cin >> intuno;
+    cout << "Ingrese segundo valor entero: ";
+    cin >> intdos;
+    cout << "Ingrese segundo valor entero: ";
+    cin >> inttres;
     
-    if(sqrt(intuno*intuno+intdos*intdos)==inttres){
+    if(sqrt((intuno*intuno)+(intdos*intdos))==inttres){
          cout << "\nSi ";
     }
-    else if (sqrt(intdos*intdos+inttres*inttres)==intuno){
+    else if (sqrt((intdos*intdos)+(inttres*inttres))==intuno){
         cout << "\nSi ";
     }
-    else if (sqrt(intuno*intuno+inttres*inttres)==intdos){
+    else if (sqrt((intuno*intuno)+(inttres*inttres))==intdos){
         cout << "\nSi ";
     }
     else {
